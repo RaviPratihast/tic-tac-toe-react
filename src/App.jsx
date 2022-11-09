6fe97a66084592ffb807a0e5a957dae003450015
RAVI SHANKAR PRATIHAST <75324945+RaviPratihast@users.noreply.github.com>
2022-11-09 17:24:26 +0530

    UI: Add head.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import "./App.css"
 
 function App(){
-    return <h1>Hello people, we are going to make tic tac toe game today!</h1>
+    return <div className="game-container">
+    {/* header */}
+    <header><h1>Tic Tac Toe</h1></header>
+
+
+    {/* game-board */}
+    <div className="game-board">
+        <div className="game-board-row game-board-row-1">
+
+            <div className="cell cell-1">x</div>
+            <div className="cell cell-2">x</div>
+            <div className="cell cell-3">x</div>
+
+        </div>
+        <div className="game-board-row game-board-row-1">
+
+            <div className="cell cell-4">x</div>
+            <div className="cell cell-5">x</div>
+            <div className="cell cell-6">x</div>
+
+        </div>
+        <div className="game-board-row game-board-row-1">
+
+            <div className="cell cell-7">x</div>
+            <div className="cell cell-8">x</div>
+            <div className="cell cell-9">x</div>
+
+        </div>
+        
+    </div>
+
+    {/* footer */}
+    </div>
 }
 
 export default App;
